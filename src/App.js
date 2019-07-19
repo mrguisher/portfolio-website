428ef1ec6515ed4a5d85d82c75fa8657f4304f28
Mateusz <mrpeciak@gmail.com>
2019-07-19 10:16:21 +0200

    add components structure to App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
 import './App.css';
+import HomePage from './components/HomePage/HomePage';
+import Portfolio from './components/Portfolio/Portfolio';
+import Navigation from './components/Navigation/Navigation';
+import AboutMe from './components/AboutMe/AboutMe';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Fragment>
+    <Navigation></Navigation>
+      <div className="main-wrapper">
+        <HomePage></HomePage>
+        <Portfolio></Portfolio>
+        <AboutMe></AboutMe>
+      </div>
+    </Fragment>
+    
+    
+
   );
 }
-
 export default App;
