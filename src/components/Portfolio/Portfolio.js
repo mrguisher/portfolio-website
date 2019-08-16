572574213484c0ddd8fb93a248b577ce1bc5a056
Mateusz <mrpeciak@gmail.com>
2019-08-16 09:57:21 +0200

    import images, change background color

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -2,16 +2,45 @@ import React, { Component } from 'react';
 import './Portfolio.css';
 import Project from '../Project/Project';
 
+import weatherCenter1 from './img/weather-app/weather1.png';
+import weatherCenter2 from './img/weather-app/weather2.png';
+import weatherCenter3 from './img/weather-app/weather3.png';
+
 class Portfolio extends Component{
+
 
   render() {
     return (
       <div className="portfolio">
         <Project
-          title="pierwszy projekt"
+          imageLeft={weatherCenter3} 
+          imageCenter={weatherCenter1}
+          imageRight={weatherCenter2}
+          title="turn-timer"
           description="krótki opis projektu - short project description"
         ></Project>
-
+        <Project
+          imageBack=""
+          imageCenter=""
+          imageFront=""
+          title="weather app"
+          description="krótki opis projektu - short project description"
+        ></Project>
+        <Project
+          imageBack=""
+          imageCenter=""
+          imageFront=""
+          title="e-commerce blogsite"
+          description="krótki opis projektu - short project description"
+        ></Project>
+        <Project
+          imageBack=""
+          imageCenter=""
+          imageFront=""
+          title="music band website"
+          description="krótki opis projektu - short project description"
+        ></Project>
+        
       </div>
     );
   }
