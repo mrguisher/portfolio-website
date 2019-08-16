413443f7840b4ab25fa57f3a1cdc56ee55e26a53
Mateusz <mrpeciak@gmail.com>
2019-08-16 08:04:47 +0200

    add Project component layout, add buttons

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './Project.css';
+import ButtonCta from '../ButtonCta/ButtonCta';
+import '../ButtonCta/ButtonCta.css'
 
 class Project extends Component{
 
@@ -7,12 +9,17 @@ class Project extends Component{
     return (
       <div className="project">
         <div className="project__img-container">
-          <img src={this.props.imageBack} alt="" className="project__image-back"/>
-          <img src={this.props.imageCenter} alt="" className="project__image-center"/>
-          <img src={this.props.imageFront} alt="" className="project__image-front"/>
+          <img src={this.props.imageLeft} alt="weather-app img" className="project__image project__image-left"/>
+          <img src={this.props.imageCenter} alt="weather-app img" className="project__image-center project__image"/>
+          <img src={this.props.imageRight} alt="weather-app img" className="project__image"/>
         </div>
         <h3 className="project__title">{this.props.title}</h3>
         <p className="project__description">{this.props.description}</p>
+        <div className="btn-container">
+          <ButtonCta className="btn btn-primary">Watch demo</ButtonCta>
+          <ButtonCta className="btn btn-secondary">Show on Github</ButtonCta>     
+        </div>
+        
       </div>
     );
   }
