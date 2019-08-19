aba65df2fe01d3a413ce8dd6996e5178dbdbe880
Mateusz <mrpeciak@gmail.com>
2019-08-19 12:45:22 +0200

    add text typing animation

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import './HomePage.css';
 
+import Typing from 'react-typing-animation';
+import Cursor from 'react-typing-animation/dist/Cursor';
+
 class HomePage extends Component{
 
   constructor() {
     super();
+
+    this.state = {
+      cursorActive: false
+    }
+   
+  }
+
+  finishedTyping = () => {
+    setTimeout(() => {
+      this.setState({cursorActive: true})
+    }, 300)
+    
   }
 
   render() {
@@ -14,8 +29,11 @@ class HomePage extends Component{
        
         <header className="main__header">
           <h1 className="main__heading-primary">Mateusz Pęciak</h1>
-          <span className="main__heading-sub">&#x0007B; web developer portfolio &#x0007D;</span>
-          <div className="code-line">&#x0007C;</div>
+          <Typing speed={120} hideCursor={false} onFinishedTyping={this.finishedTyping}>
+            <span className="main__heading-sub">&#x0007B; web dev<Typing.Speed ms={50} />eloper  portfo<Typing.Speed ms={130}/>lio &#x0007D;</span>
+          </Typing>
+
+         {this.state.cursorActive && <div className="code-line">&#x0007C;</div>}
         </header>
 
       </div>
