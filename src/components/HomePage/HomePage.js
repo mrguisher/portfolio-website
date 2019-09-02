66713bd76f8f2522f496c9757c2d47f3a6589efd
Mateusz <mrpeciak@gmail.com>
2019-09-02 09:54:56 +0200

    add media queries to homepage section

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './HomePage.css';
 
 import Typing from 'react-typing-animation';
-import Cursor from 'react-typing-animation/dist/Cursor';
 
 class HomePage extends Component{
 
@@ -12,7 +11,6 @@ class HomePage extends Component{
     this.state = {
       cursorActive: false
     }
-   
   }
 
   finishedTyping = () => {
@@ -25,14 +23,11 @@ class HomePage extends Component{
   render() {
     return (
       <div className="main" id={this.props.id}>
-        
-       
         <header className="main__header">
           <h1 className="main__heading-primary">Mateusz Pęciak</h1>
           <Typing speed={120} hideCursor={false} onFinishedTyping={this.finishedTyping}>
             <span className="main__heading-sub">&#x0007B; web dev<Typing.Speed ms={50} />eloper  portfo<Typing.Speed ms={130}/>lio &#x0007D;</span>
           </Typing>
-
          {this.state.cursorActive && <div className="code-line">&#x0007C;</div>}
         </header>
 
