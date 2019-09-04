d87c2dce724d7760ce5688f8fd789ef0999468c1
Mateusz <mrpeciak@gmail.com>
2019-09-04 15:30:23 +0200

    add about section media queries

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -12,27 +12,28 @@ class AboutMe extends Component{
 
   render() {
     return (
+      <React.Fragment>
       <div className="about" id={this.props.id}>
         <div className="about__photo-rounded">
-          <img src={photo} alt="photo" className="about__photo"/>
+          <img src={photo} alt="" className="about__photo"/>
         </div>
         <div className="center-box">
-        <ScrollAnimation animateOnce="true" animateIn="fadeIn" delay={200}><h2 className="about__heading">Hello</h2></ScrollAnimation>
-        <ScrollAnimation animateOnce="true" animateIn="fadeIn" delay={500}><p className="about__text-line">I'm Mateusz</p></ScrollAnimation>
-        <ScrollAnimation animateOnce="true" animateIn="fadeIn" delay={800}><p className="about__text-line">frontend developer</p></ScrollAnimation>
-        <ScrollAnimation animateOnce="true" animateIn="fadeIn" delay={1200}><p className="about__text-line">feel free to contact me at <span className="about__bold">mrpeciak@gmail.com</span> </p></ScrollAnimation>
-          
+          <ScrollAnimation animateOnce="true" animateIn="fadeIn" delay={100}><h2 className="about__heading">Hello</h2></ScrollAnimation>
+          <ScrollAnimation animateOnce="true" animateIn="fadeIn" delay={400}><p className="about__text-line">I'm Mateusz</p></ScrollAnimation>
+          <ScrollAnimation animateOnce="true" animateIn="fadeIn" delay={700}><p className="about__text-line">frontend developer</p></ScrollAnimation>
+          <ScrollAnimation animateOnce="true" animateIn="fadeIn" delay={1000}><p className="about__text-line">feel free to contact me at <span className="about__bold">mrpeciak@gmail.com</span> </p></ScrollAnimation>
         </div>
-        
         <div className="about__socials">
-        
-               <a href="https://github.com/mrguisher" className="socials-item"><img src={github} alt="github-link" /></a>
-               <a href="https://www.linkedin.com/in/mateuszpeciak/" className="socials-item"><img src={linkedin} alt="linkedin-link" /></a>
-               <ScrollAnimation animateOnce="true" animateIn="fadeIn" delay={500}><a href={`${process.env.PUBLIC_URL}/cv`} className="about__text-line">download my <span className="about__bold">Curriculum Vitae</span></a></ScrollAnimation>
-        
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/mrguisher" className="socials-item"><img src={github} alt="github-link" /></a>
+          <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/mateuszpeciak/" className="socials-item"><img src={linkedin} alt="linkedin-link" /></a>
+          <a target="_blank" rel="noopener noreferrer" href={`${process.env.PUBLIC_URL}assets/CV_Mateusz_Pęciak.pdf`} className="about__text-line">download my <span className="about__bold">Curriculum Vitae</span></a>
         </div>  
-        
       </div>
+      <div className="text-small-box">
+        <div className="text-small">Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"     title="Flaticon">www.flaticon.com</a></div>
+        <div className="text-small">Icons made by <a href="https://www.flaticon.com/authors/linh-pham" title="Linh Pham">Linh Pham</a> from <a href="https://www.flaticon.com/"     title="Flaticon">www.flaticon.com</a></div>
+      </div> 
+      </React.Fragment>
     );
   }
 }
